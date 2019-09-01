40e1ffe24076707903d70bc866099298d7b96ab0
groeschn <groeschn@gmx.de>
2019-09-01 11:33:44 +0200

    reduce equals and hashcode to id

--- src/main/java/de/groeschn/javaee/model/TimerInfo.java
+++ src/main/java/de/groeschn/javaee/model/TimerInfo.java
@@ -1,13 +1,16 @@
 package de.groeschn.javaee.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.math.BigInteger;
 import java.time.ZonedDateTime;
 
 @Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class TimerInfo {
 
+    @EqualsAndHashCode.Include
     private String id;
     private String name;
     private BigInteger accumulatedMilliseconds;
